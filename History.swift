f8a0960c7b807cfb1e987b68755292bd1f21f916
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-30 16:48:33 +0530

    :Rename

--- History.swift
+++ History.swift
@@ -5,9 +5,3 @@
 //? The value of a constant can’t be changed once it’s set, whereas a variable can be set to a different value in the future
 //? Constants and variables must be declared before they’re used. You declare constants with the let keyword and variables with the var keyword
 //? Here’s an example of how constants and variables can be used to track the number of login attempts a user has made:
-
-let Fname = "Bhavin";
-var Lname = "Pathak";
-
-print(Fname);
-print(Lname);
