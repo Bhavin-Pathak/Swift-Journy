7d4c784bec3125df223783c1c309767da0b39997
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-30 16:46:20 +0530

    Diff: Basics Of Swift Added

---
+++ Basics.swift
@@ -0,0 +1,13 @@
+//? Swift provides many fundamental data types, 
+//! Including Int for integers, Double for floating-point values, Bool for Boolean values, and String for text. 
+
+//! Constants and Variables
+//? The value of a constant can’t be changed once it’s set, whereas a variable can be set to a different value in the future
+//? Constants and variables must be declared before they’re used. You declare constants with the let keyword and variables with the var keyword
+//? Here’s an example of how constants and variables can be used to track the number of login attempts a user has made:
+
+let name = "Mansi";
+var Sname = "Mahant";
+
+print(name);
+print(Sname);
