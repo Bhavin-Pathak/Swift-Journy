04f366f66107ef12c25c1c081286f59003d25b8a
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-31 18:42:52 +0530

    :

--- History.swift
+++
@@ -1,7 +0,0 @@
-//? Swift provides many fundamental data types, 
-//! Including Int for integers, Double for floating-point values, Bool for Boolean values, and String for text. 
-
-//! Constants and Variables
-//? The value of a constant can’t be changed once it’s set, whereas a variable can be set to a different value in the future
-//? Constants and variables must be declared before they’re used. You declare constants with the let keyword and variables with the var keyword
-//? Here’s an example of how constants and variables can be used to track the number of login attempts a user has made:
