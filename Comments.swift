0ccb0eddfa4d2c89cc3f16ec7a9fc3f1214fdf43
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-31 19:53:25 +0530

    Chore: Comments in swift Added

---
+++ Comments.swift
@@ -0,0 +1,20 @@
+//! Swift Comments
+
+//? There are two ways to add comments in Swift:
+
+// - Single Line comments
+/*...*/  :- //- Multiline Comments
+
+// create a variable 
+var name = "Cartman"
+
+// print the value
+print(name)
+
+/* create a variable
+to store salary of employees
+*/
+
+var salary = 10000
+print(salary)
+
