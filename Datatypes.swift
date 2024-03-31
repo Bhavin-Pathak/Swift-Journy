eb2dd682252068f3ca81a40f080c198e917cdbdb
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-31 19:03:46 +0530

    Chore: DataTypes Added

---
+++ Datatypes.swift
@@ -0,0 +1,57 @@
+//! Swift Data Types
+
+//? Data Types	Example	Description
+
+//! Character	"s","a"	 :- a 16-bit Unicode character
+//! String	"hello world!"	:- represents textual data
+//! Int	3, -23	:- an integer number
+//! Float	2.4, 3.14, -23.21	:- represents 32-bit floating-point number
+//! Double	2.422342412414	:- represents 64-bit floating-point number
+//! Bool	true and false	:- Any of two values: true or false
+
+//? Character Data type
+//! The character data type is used to represent a single-character string. We use the Character keyword to create character-type variables. For example
+// create character type variable
+var letter: Character = "s"
+print(letter)
+// Output: s
+//? String Data type
+//! The string data type is used to represent textual data. We use the String keyword to create string-type variables. For example,
+// create string type variable
+var language: String = "swift"
+print(language)
+// Output: swift
+//? Int Data type
+//! An integer data type is used to represent a whole number with no fractional component. We use the Int keyword to create integer-type variables. For example,
+// create integer type variable
+var number: Int = 3
+print(number)
+// Output: 3
+
+//? Float Data type
+//! A float data type is used to represent a number with a fractional component. We use the Float keyword to create float-type variables. For example
+// create float type variable
+let piValue: Float = 3.14
+print(piValue)
+// Output: 3.14
+
+//? Double Data type
+//! Like Float, a double data type is also used to represent a number with fractional components. However, Double supports data up to 15 decimal places. We use the Double keyword to create double variables. For example,
+// create double type variable
+let latitude: Double = 27.7007697012432
+print(latitude)
+// Output: 27.7007697012432
+
+//? Bool Data type
+//! A boolean data type is used to represent logical entities. It can have one of two values: true or false. We use the Bool keyword to create boolean-type variables. For example
+// create boolean type variable
+let passCheck: Bool = true
+print(passCheck)
+let failCheck: Bool = false
+print(failCheck)
+// Output: true
+// Output: false
+
+
+
+
