35cff1f702780365efaa8041cfe06626c72be884
Bhavin8289 <bupadhyay277@gmail.com>
2024-05-03 20:22:39 +0530

    init

--- Udemy-Stuffs.playground/Contents.swift
+++ Udemy-Stuffs.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var greeting = "Hello, playground !!"
+
+//MARK: DataTypes With Type Annotaction
+var no: Int
+var name : String
+var age : Double
+var isMan: Bool
+var str: Character
+
+name = "Bhavin"
+print(name)
+
+
